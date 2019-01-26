d5ae88889675089d60fa120c22dcb4fb22b4112d
MininMobile <notminin@pm.me>
2019-01-26 15:49:07 +0000

    add toggleable panels

--- src/irrumabo.js
+++ src/irrumabo.js
@@ -6,8 +6,21 @@ let Runner = Matter.Runner;
 let World = Matter.World;
 let Bodies = Matter.Bodies;
 
-let paused = false;
+// get buttons
+let buttonPause = document.getElementById("pause");
+let buttonSettings = document.getElementById("settings");
+let buttonTools = document.getElementById("tools");
+let buttonComponents = document.getElementById("components");
+// get panels
+let panelTools = document.getElementById("tools-panel");
+let panelToolOptions = document.getElementById("tools-option-panel");
+let panelComponents = document.getElementById("components-panel");
 
+let paused = false;
+let toolsToggle = true;
+let componentsToggle = true;
+
+// create enviroment
 let engine = Engine.create();
 
 let render = Render.create({
@@ -29,19 +42,21 @@ let runner = Runner.create({
 
 // create example scene
 let ballA = Bodies.circle(400, 200, 40, { density: 0.001 });
-let ballB = Bodies.circle(450, 50, 40, { density: 1 });
+let ballB = Bodies.circle(500, 200, 40, { density: 1 });
 let ground = Bodies.rectangle(document.body.scrollWidth / 2, document.body.scrollHeight / 2, document.body.scrollWidth, 60, { isStatic: true, angle: Math.PI * 0.06 });
 
 World.add(engine.world, [ballA, ballB, ground]);
 
+// add button actions
+buttonPause.addEventListener("click", togglePaused);
+buttonSettings.addEventListener("click", () => {});
+buttonTools.addEventListener("click", () => { panelTools.classList.toggle("hidden"); panelToolOptions.classList.toggle("hidden") });
+buttonComponents.addEventListener("click", () => { panelComponents.classList.toggle("hidden") });
+
 // add keyboard shortcuts
 document.addEventListener("keyup", (e) => {
 	switch (e.code) {
-		case "Space": {
-			paused = !paused;
-
-			runner.enabled = !paused;
-		} break;
+		case "Space": togglePaused(); break;
 	}
 });
 
@@ -58,9 +73,13 @@ let mouseConstraint = MouseConstraint.create(engine, {
 });
 
 World.add(engine.world, mouseConstraint);
-
 render.mouse = mouse;
 
-// run
 Runner.run(runner, engine);
 Render.run(render);
+
+function togglePaused() {
+	paused = !paused;
+
+	runner.enabled = !paused;
+}
