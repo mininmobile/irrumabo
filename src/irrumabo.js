0b10380e0fa7f87d7992a39dfe9155dd89bdaf36
MininMobile <notminin@pm.me>
2019-01-26 14:03:44 +0000

    add pausing, unminimize matterjs

--- src/irrumabo.js
+++ src/irrumabo.js
@@ -6,6 +6,8 @@ let Runner = Matter.Runner;
 let World = Matter.World;
 let Bodies = Matter.Bodies;
 
+let paused = false;
+
 let engine = Engine.create();
 
 let render = Render.create({
@@ -26,11 +28,22 @@ let runner = Runner.create({
 });
 
 // create example scene
-let boxA = Bodies.rectangle(400, 200, 80, 80);
-let boxB = Bodies.rectangle(450, 50, 80, 80);
-let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+let ballA = Bodies.circle(400, 200, 40, { density: 0.001 });
+let ballB = Bodies.circle(450, 50, 40, { density: 1 });
+let ground = Bodies.rectangle(document.body.scrollWidth / 2, document.body.scrollHeight / 2, document.body.scrollWidth, 60, { isStatic: true, angle: Math.PI * 0.06 });
 
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, [ballA, ballB, ground]);
+
+// add keyboard shortcuts
+document.addEventListener("keyup", (e) => {
+	switch (e.code) {
+		case "Space": {
+			paused = !paused;
+
+			runner.enabled = !paused;
+		} break;
+	}
+});
 
 // add mouse control
 let mouse = Mouse.create(render.canvas);
